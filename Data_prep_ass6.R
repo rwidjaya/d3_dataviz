ff717906ee7c4b8acd656df531d8db63673bd545
Cynette791 <reginalwidjaya@gmail.com>
2017-11-06 18:00:34 -0600

    Made the inversion

--- Data_prep_ass6.R
+++ Data_prep_ass6.R
@@ -1,10 +1,18 @@
-h16 <- read_excel('H-1B_Disclosure_Data_FY16.xlsx') 
+library(readr)
+library(haven)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(ggplot2)
+library(readxl)
+
+h16 <- read_excel('../../DATA/h1b/H-1B_Disclosure_Data_FY16.xlsx') 
 h16 <- filter(h16, CASE_STATUS == "CERTIFIED" & WAGE_UNIT_OF_PAY =="Year")
 
 h16_sub <- select(h16, c("CASE_NUMBER", "VISA_CLASS", "EMPLOYER_NAME", "EMPLOYER_STATE", "NAIC_CODE", "PREVAILING_WAGE"))
-h16_spl <- h16_sub[sample(1:nrow(h16_sub), 200, replace=FALSE),]
+h16_spl <- h16_sub[sample(1:nrow(h16_sub), 300, replace=FALSE),]
 
-naic <- read_excel('ISIC_4_to_2017_NAICS.xlsx')
+naic <- read_excel('../../DATA/ISIC_4_to_2017_NAICS.xlsx')
 naic <- select(naic, c("2017\r\nNAICS\r\nUS", "2017 NAICS US TITLE"))
 naic <- plyr::rename(naic, c("2017\r\nNAICS\r\nUS" = "NAIC_CODE", "2017 NAICS US TITLE" ="NAIC_CODE_TITLE"))
 naic <- unique(naic)
@@ -15,3 +23,8 @@ h16_spl <- left_join(h16_spl, naic)
 h16_spl <- h16_spl[complete.cases(h16_spl[ , -1]),]
 
 h16_fnl <- select(h16_spl, c("CASE_NUMBER", "PREVAILING_WAGE", "NAIC_CODE_TITLE" ))
+h16_fnl_2 <- h16_fnl %>%
+  group_by(NAIC_CODE_TITLE) %>%
+  summarise(MED_PW = median(PREVAILING_WAGE), TOTAL = n())
+h16_fnl_2$MED_PW <- scales::dollar(h16_fnl_2$MED_PW)
+
