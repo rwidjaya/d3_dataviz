c28021b5c153852dd0589f16cd582bffe7f52a4d
Cynette791 <reginalwidjaya@gmail.com>
2017-11-08 01:58:28 -0600

    Adding Color Class

--- Data_prep_ass6.R
+++ Data_prep_ass6.R
@@ -5,18 +5,20 @@ library(tidyr)
 library(stringr)
 library(ggplot2)
 library(readxl)
+library(jsonlite)
 
 h16 <- read_excel('../../DATA/h1b/H-1B_Disclosure_Data_FY16.xlsx') 
 h16 <- filter(h16, CASE_STATUS == "CERTIFIED" & WAGE_UNIT_OF_PAY =="Year")
 
 h16_sub <- select(h16, c("CASE_NUMBER", "VISA_CLASS", "EMPLOYER_NAME", "EMPLOYER_STATE", "NAIC_CODE", "PREVAILING_WAGE"))
-h16_spl <- h16_sub[sample(1:nrow(h16_sub), 300, replace=FALSE),]
+
 
 naic <- read_excel('../../DATA/ISIC_4_to_2017_NAICS.xlsx')
 naic <- select(naic, c("2017\r\nNAICS\r\nUS", "2017 NAICS US TITLE"))
 naic <- plyr::rename(naic, c("2017\r\nNAICS\r\nUS" = "NAIC_CODE", "2017 NAICS US TITLE" ="NAIC_CODE_TITLE"))
 naic <- unique(naic)
 
+h16_spl <- h16_sub[sample(1:nrow(h16_sub), 200, replace=FALSE),]
 h16_spl$NAIC_CODE <- as.numeric(h16_spl$NAIC_CODE)
 
 h16_spl <- left_join(h16_spl, naic)
@@ -26,5 +28,8 @@ h16_fnl <- select(h16_spl, c("CASE_NUMBER", "PREVAILING_WAGE", "NAIC_CODE_TITLE"
 h16_fnl_2 <- h16_fnl %>%
   group_by(NAIC_CODE_TITLE) %>%
   summarise(MED_PW = median(PREVAILING_WAGE), TOTAL = n())
-h16_fnl_2$MED_PW <- scales::dollar(h16_fnl_2$MED_PW)
+h16_fnl_2$MED_PW <- round(h16_fnl_2$MED_PW)
+
+h16_json <- toJSON(h16_fnl_2, pretty = TRUE)
+write(h16_json, "negative.json")
 
